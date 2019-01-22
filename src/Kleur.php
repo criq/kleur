692b196e984f553979e9201468bf7268c0ce7d5c
Jan Jablunka <jablunka@gmail.com>
2019-01-22 11:32:33 +0100

    incremental

--- src/Kleur.php
+++ src/Kleur.php
@@ -24,36 +24,6 @@ class Kleur {
 		return $color;
 	}
 
-	static function extractColors($uri, $n = 1) {
-		try {
-			return \Katu\Utils\Cache::get(['image', 'color', 'extract', $uri, $n], function() use($uri, $n) {
-				$client = new \League\ColorExtractor\Client;
-
-				$image = \Katu\Utils\Image::getVersionUrl($uri, [
-					'filters' => [
-						[
-							'filter' => 'fit',
-							'width' => 600,
-							'height' => 600,
-						],
-					],
-				]);
-
-				$extracted = (array) $client->loadJpeg($image)->extract($n);
-
-				return array_map(function($i) {
-					$color = \Katu\Types\TColorRgb::getFromHex($i);
-
-					return new \MischiefCollective\ColorJizz\Formats\RGB($color->r, $color->g, $color->b);
-				}, $extracted);
-			});
-		} catch (\Exception $e) {
-			\Katu\ErrorHandler::log($e);
-
-			return false;
-		}
-	}
-
 	static function orderColorsByBrightness($colors) {
 		$sorted = [];
 
